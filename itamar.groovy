9f28d4d7cbd06d2349110cea8eeec98ca6902a19
8dots <BlueDevOps@gmail.com>
2018-10-03 16:53:59 +0300

    Update itamar.groovy

--- itamar.groovy
+++ itamar.groovy
@@ -7,7 +7,7 @@ pipeline {
             }        
             steps {
                 sh 'sudo service mongod start'
-                sh 'sudo npm install'
+                sh 'sudo npm install -g node-pre-gyp'
                 sh 'npm run test:coverage'
             }
         }        
