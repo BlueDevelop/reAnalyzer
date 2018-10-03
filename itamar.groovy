9ae0959ee6a794816743f8f39d623cb49e1c1106
Omer Zamir <zamir98@gmail.com>
2018-10-03 17:11:23 +0300

    Update itamar.groovy

--- itamar.groovy
+++ itamar.groovy
@@ -8,7 +8,10 @@ pipeline {
             steps {
                 sh 'sudo service mongod start'
                 sh 'sudo npm install -g node-pre-gyp'
-                sh 'sudo npm install'
+                sh 'sudo npm install -g typescript'
+                sh 'sudo npm install -g nyc'
+                sh 'sudo npm install -g mocha'
+                sh 'npm install'
                 sh 'npm run test:coverage'
             }
         }        
