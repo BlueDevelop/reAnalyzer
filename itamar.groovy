d76f61d057b407828639c1db13175f992d5039f8
Omer Zamir <zamir98@gmail.com>
2018-10-03 17:16:52 +0300

    Update itamar.groovy

--- itamar.groovy
+++ itamar.groovy
@@ -6,11 +6,8 @@ pipeline {
                 label 'reanalyzer-backend'
             }        
             steps {
+                sh 'sudo apt-get install build-essential'
                 sh 'sudo service mongod start'
-                sh 'sudo npm install -g node-pre-gyp'
-                sh 'sudo npm install -g typescript'
-                sh 'sudo npm install -g nyc'
-                sh 'sudo npm install -g mocha'
                 sh 'npm install'
                 sh 'npm run test:coverage'
             }
