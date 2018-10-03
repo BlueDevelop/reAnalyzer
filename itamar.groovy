918664fcc2b6afcf829277e93ae8f1be0d2ab02c
Omer Zamir <zamir98@gmail.com>
2018-10-03 17:23:34 +0300

    Update itamar.groovy

--- itamar.groovy
+++ itamar.groovy
@@ -8,6 +8,7 @@ pipeline {
             steps {
                 sh 'sudo apt-get install build-essential -y'
                 sh 'sudo service mongod start'
+                sh 'npm install -g node-gyp'
                 sh 'npm install'
                 sh 'npm run test:coverage'
             }
