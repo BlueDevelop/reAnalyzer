165ef3b81a7a47055a5e20e8dfef1ef7c6a68cf1
Omer Zamir <zamir98@gmail.com>
2018-10-04 09:59:56 +0300

    Update itamar.groovy

--- itamar.groovy
+++ itamar.groovy
@@ -6,8 +6,7 @@ pipeline {
                 label 'reanalyzer-backend'
             }        
             steps {
-                sh 'sudo apt-get install build-essential libkrb5-dev -y'
-                sh 'rm -rf $HOME/.node-gyp'
+                sh 'sudo apt-get install build-essential -y'
                 sh 'sudo service mongod start'
                 sh 'sudo npm install -g node-gyp node-pre-gyp'
                 sh 'npm install'
