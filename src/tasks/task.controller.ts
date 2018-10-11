78b13761dbea9d0656e0da9a7fa2adb78eccb8c7
Omer <zamir98@gmail.com>
2018-10-11 09:09:59 +0300

    added functions to the controller

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,5 +1,39 @@
 import taskService from './task.service';
+import { Request, Response, NextFunction } from 'express';
 
-export default class taskController extends taskService {
-    
+export default class taskController {
+
+    static async getFieldCountPerInterval(req: Request, res: Response, next: NextFunction) {
+        if (!req.query.field || !req.query.from || !req.query.to) {
+            return res.status(400);
+        }
+        if(req.query.field !== 'due' && req.query.field !== 'created') {
+            return res.status(400);
+        }
+        if(isNaN(req.query.from) || isNaN(req.query.to)) {
+            return res.status(400);
+        }
+
+        const response = await taskService.getFieldCountPerInterval(req.query.field,
+                                                                    +req.query.from,
+                                                                    +req.query.to,
+                                                                    req.query.interval);
+
+        res.json(response.hits.hits);
+        return next(response.hits.hits);
+    }
+
+    static async getCountByStatus(req: Request, res: Response, next: NextFunction) {
+        if (!req.query.from || !req.query.to) {
+            return res.status(400);
+        }
+        if(isNaN(req.query.from) || isNaN(req.query.to)) {
+            return res.status(400);
+        }
+
+        const response = await taskService.getCountByStatus(+req.query.from, +req.query.to);
+        
+        res.json(response.hits.hits);
+        return next(response.hits.hits);
+    }
 }
