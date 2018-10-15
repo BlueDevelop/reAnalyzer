57a581e539f5b49fb28a8a88aed213ab660f2da2
Omri <omrihaber@me.com>
2018-10-15 14:42:55 +0300

    linter and prettier

--- src/helpers/userhierarchy.helper.ts
+++ src/helpers/userhierarchy.helper.ts
@@ -10,21 +10,22 @@ import * as fs from "fs";
  */
 async function getHierarchy(rootHierarchy?: string) {
   if (config.hierarchyServiceAddrGetMembers && rootHierarchy) {
-    //if theres an api to get the hirerchy from and a root hirerchy to get then make then make the request to the api
+    // if theres an api to get the hirerchy from and a root hirerchy to get then make then make the request to the api
     const getURL: string = config.hierarchyServiceAddrGetMembers(rootHierarchy);
     const response = await axios.get(getURL);
     const resData = response.data;
+
     const idList = _.map(resData, person => person.id);
     return idList;
   } else if (config.hierarchyServiceMockFile) {
-    //use a static mock json file
+    // use a static mock json file
     const data = JSON.parse(
       fs.readFileSync(config.hierarchyServiceMockFile, "utf8")
     );
     const idList = _.map(data, person => person.id);
     return idList;
   } else {
-    //TODO:throw error or do nothing
+    // TODO:throw error or do nothing
     throw new Error("hierarchy not configured");
   }
 }
