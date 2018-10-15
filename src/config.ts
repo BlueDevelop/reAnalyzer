27c3c4657065e6b00dc226b6f847b15994a50a17
Omri <omrihaber@me.com>
2018-10-15 14:16:07 +0300

    added prettier and tsling

--- src/config.ts
+++ src/config.ts
@@ -1,67 +1,69 @@
 import hierarchyService from "./helpers/userhierarchy.helper";
 
 interface IConfig {
-    port: number,
-    connString: string,
-    sessionSecret: string,
-    logLevel: string,
-    apmAdress: string,
-    apmServiceName: string,
-    elasticsearch: string,
-    hierarchyServiceAddr?:string,
-    hierarchyServiceMockFile?:string,
-    hierarchyServiceAddrGetMembers?:(hierarchy:string)=>string,
+  port: number;
+  connString: string;
+  sessionSecret: string;
+  logLevel: string;
+  apmAdress: string;
+  apmServiceName: string;
+  elasticsearch: string;
+  hierarchyServiceAddr?: string;
+  hierarchyServiceMockFile?: string;
+  hierarchyServiceAddrGetMembers?: (hierarchy: string) => string;
 }
 
 const prod: IConfig = {
-    port: 3000,
-    connString: 'mongodb://localhost:27017,localhost:27017,localhost:27017/reAnalyzer_prod?replica=reAnalyzer',
-    sessionSecret: 'OmerIsTheBestProgrammer',
-    logLevel: 'info',
-    apmAdress: 'http://apm:8200',
-    apmServiceName: 'reAnalyzer_prod',
-    elasticsearch: 'http://elasticsearch:9200',
-    hierarchyServiceAddr:'http://hierarchyServiceAddress/api/hierarchy/',
-    hierarchyServiceAddrGetMembers:(hierarchyID:string)=>`${prod.hierarchyServiceAddr}${hierarchyID}/members`,
+  port: 3000,
+  connString:
+    "mongodb://localhost:27017,localhost:27017,localhost:27017/reAnalyzer_prod?replica=reAnalyzer",
+  sessionSecret: "OmerIsTheBestProgrammer",
+  logLevel: "info",
+  apmAdress: "http://apm:8200",
+  apmServiceName: "reAnalyzer_prod",
+  elasticsearch: "http://elasticsearch:9200",
+  hierarchyServiceAddr: "http://hierarchyServiceAddress/api/hierarchy/",
+  hierarchyServiceAddrGetMembers: (hierarchyID: string) =>
+    `${prod.hierarchyServiceAddr}${hierarchyID}/members`
 };
 
 const dev: IConfig = {
-    port: 3000,
-    connString: 'mongodb://mongodb:27017/reAnalyzer_dev',
-    sessionSecret: 'OmerIsTheBestProgrammer',
-    logLevel: 'verbose',
-    apmAdress: 'http://apm:8200',
-    apmServiceName: 'reAnalyzer_dev',
-    elasticsearch: 'http://elasticsearch:9200',
-    hierarchyServiceMockFile:"./mock/members.json"
+  port: 3000,
+  connString: "mongodb://mongodb:27017/reAnalyzer_dev",
+  sessionSecret: "OmerIsTheBestProgrammer",
+  logLevel: "verbose",
+  apmAdress: "http://apm:8200",
+  apmServiceName: "reAnalyzer_dev",
+  elasticsearch: "http://elasticsearch:9200",
+  hierarchyServiceMockFile: "./mock/members.json"
 };
 
 const test: IConfig = {
-    port: 3001,
-    connString: 'mongodb://localhost:27017/reAnalyzer_test',
-    sessionSecret: 'OmerIsTheBestProgrammer',
-    logLevel: 'verbose',
-    apmAdress: 'http://localhost:8200',
-    apmServiceName: 'reAnalyzer_test',
-    elasticsearch: 'http://localhost:9200'
+  port: 3001,
+  connString: "mongodb://localhost:27017/reAnalyzer_test",
+  sessionSecret: "OmerIsTheBestProgrammer",
+  logLevel: "verbose",
+  apmAdress: "http://localhost:8200",
+  apmServiceName: "reAnalyzer_test",
+  elasticsearch: "http://localhost:9200"
 };
 
 export default () => {
-    switch (process.env.NODE_ENV) {
-        case 'prod': {
-            return prod;
-        }
-
-        case 'dev': {
-            return dev;
-        }
-
-        case 'test': {
-            return test;
-        }
-
-        default: {
-            return dev;
-        }
+  switch (process.env.NODE_ENV) {
+    case "prod": {
+      return prod;
     }
-}
+
+    case "dev": {
+      return dev;
+    }
+
+    case "test": {
+      return test;
+    }
+
+    default: {
+      return dev;
+    }
+  }
+};
--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,118 +1,135 @@
-import taskService from './task.service';
-import infoLogger from '../loggers/info.logger';
-import errorLogger from '../loggers/error.logger';
-import { Request, Response, NextFunction } from 'express';
+import taskService from "./task.service";
+import infoLogger from "../loggers/info.logger";
+import errorLogger from "../loggers/error.logger";
+import { Request, Response, NextFunction } from "express";
 
 export default class taskController {
+  /**
+   * validates queries and fetch the count of tasks per interval as given by a given field in given time range.
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   */
+  static async getFieldCountPerInterval(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      if (!req.query.field || !req.query.from || !req.query.to) {
+        res.status(400);
+        return next();
+      }
+      if (req.query.field !== "due" && req.query.field !== "created") {
+        res.status(400);
+        return next();
+      }
+      if (isNaN(req.query.from) || isNaN(req.query.to)) {
+        res.status(400);
+        return next();
+      }
 
-    /**
-     * validates queries and fetch the count of tasks per interval as given by a given field in given time range.
-     * 
-     * @param {Request} req
-     * @param {Response} res
-     * @param {NextFunction} next
-     */
-    static async getFieldCountPerInterval(req: Request, res: Response, next: NextFunction) {
-        try {
-            if (!req.query.field || !req.query.from || !req.query.to) {
-                res.status(400);
-                return next();
-            }
-            if (req.query.field !== 'due' && req.query.field !== 'created') {
-                res.status(400);
-                return next();
-            }
-            if (isNaN(req.query.from) || isNaN(req.query.to)) {
-                res.status(400);
-                return next();
-            }
+      const response = await taskService.getFieldCountPerInterval(
+        req.query.field,
+        +req.query.from,
+        +req.query.to,
+        req.query.interval
+      );
 
-            const response = await taskService.getFieldCountPerInterval(req.query.field,
-                +req.query.from,
-                +req.query.to,
-                req.query.interval);
-
-            res.json(response.aggregations['1']['buckets']);
-            return next(response.aggregations['1']['buckets']);
-        }
-        catch (err) {
-            errorLogger.error('%j', {
-                message: err.message,
-                stack: err.stack,
-                name: err.name
-            });
-            res.status(500);
-            next(err);
-        }
+      res.json(response.aggregations["1"]["buckets"]);
+      return next(response.aggregations["1"]["buckets"]);
+    } catch (err) {
+      errorLogger.error("%j", {
+        message: err.message,
+        stack: err.stack,
+        name: err.name
+      });
+      res.status(500);
+      next(err);
     }
+  }
 
-    /**
-     * validates queries and fetch the count of tasks per status in a given time range.
-     * 
-     * @param {Request} req
-     * @param {Response} res
-     * @param {NextFunction} next
-     */
-    static async getCountByStatus(req: Request, res: Response, next: NextFunction) {
-        try {
-            if (!req.query.from || !req.query.to) {
-                res.status(400);
-                return next();
-            }
-            if (isNaN(req.query.from) || isNaN(req.query.to)) {
-                res.status(400);
-                return next();
-            }
+  /**
+   * validates queries and fetch the count of tasks per status in a given time range.
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   */
+  static async getCountByStatus(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      if (!req.query.from || !req.query.to) {
+        res.status(400);
+        return next();
+      }
+      if (isNaN(req.query.from) || isNaN(req.query.to)) {
+        res.status(400);
+        return next();
+      }
 
-            const response = await taskService.getCountByStatus(+req.query.from, +req.query.to);
+      const response = await taskService.getCountByStatus(
+        +req.query.from,
+        +req.query.to
+      );
 
-            res.json(response.aggregations['1']['buckets']);
-            return next(response.aggregations['1']['buckets']);
-        }
-        catch (err) {
-            errorLogger.error('%j', {
-                message: err.message,
-                stack: err.stack,
-                name: err.name
-            });
-            res.status(500);
-            next(err);
-        }
+      res.json(response.aggregations["1"]["buckets"]);
+      return next(response.aggregations["1"]["buckets"]);
+    } catch (err) {
+      errorLogger.error("%j", {
+        message: err.message,
+        stack: err.stack,
+        name: err.name
+      });
+      res.status(500);
+      next(err);
     }
+  }
 
-    /**
-     * validates queries and fetch the cloud tag data in a given time range.
-     * 
-     * @param {Request} req
-     * @param {Response} res
-     * @param {NextFunction} next
-     */
-    static async getTagCloud(req: Request, res: Response, next: NextFunction) {
-        try {
-            if (!req.query.from || !req.query.to) {
-                res.status(400);
-                return next();
-            }
-            if (isNaN(req.query.from) || isNaN(req.query.to) || (req.query.size && isNaN(req.query.size))) {
-                res.status(400);
-                return next();
-            }
+  /**
+   * validates queries and fetch the cloud tag data in a given time range.
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   */
+  static async getTagCloud(req: Request, res: Response, next: NextFunction) {
+    try {
+      if (!req.query.from || !req.query.to) {
+        res.status(400);
+        return next();
+      }
+      if (
+        isNaN(req.query.from) ||
+        isNaN(req.query.to) ||
+        (req.query.size && isNaN(req.query.size))
+      ) {
+        res.status(400);
+        return next();
+      }
 
-            const size = req.query.size ? +req.query.size : undefined;
+      const size = req.query.size ? +req.query.size : undefined;
 
-            const response = await taskService.getTagCloud(+req.query.from, +req.query.to, size);
+      const response = await taskService.getTagCloud(
+        +req.query.from,
+        +req.query.to,
+        size
+      );
 
-            res.json(response.aggregations['1']['buckets']);
-            return next(response.aggregations['1']['buckets']);
-        }
-        catch (err) {
-            errorLogger.error('%j', {
-                message: err.message,
-                stack: err.stack,
-                name: err.name
-            });
-            res.status(500);
-            next(err);
-        }
+      res.json(response.aggregations["1"]["buckets"]);
+      return next(response.aggregations["1"]["buckets"]);
+    } catch (err) {
+      errorLogger.error("%j", {
+        message: err.message,
+        stack: err.stack,
+        name: err.name
+      });
+      res.status(500);
+      next(err);
     }
+  }
 }
