8d69d867d18657199e699f54466b6077713c6c81
Omer <zamir98@gmail.com>
2018-10-16 18:51:47 +0300

    "Finish ratio"
    not finished

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -2,6 +2,7 @@ import taskService from './task.service';
 import infoLogger from '../loggers/info.logger';
 import errorLogger from '../loggers/error.logger';
 import { Request, Response } from 'express';
+import * as _ from 'lodash';
 
 export default class TaskController {
   /**
@@ -145,5 +146,95 @@ export default class TaskController {
       });
       return res.status(500);
     }
+  }
+
+  /**
+   * validates queries and fetch the task finish ratio in a given time range.
+   *
+   * @param {Request} req
+   * @param {Response} res
+   */
+  public static async getEndTimeRatio(req: Request, res: Response) {
+    try {
+      if (!req.query.from || !req.query.to) {
+        return res.status(400);
+      }
+      if (
+        isNaN(req.query.from) ||
+        isNaN(req.query.to) ||
+        (req.query.size && isNaN(req.query.size))
+      ) {
+        return res.status(400);
+      }
+
+      const doneTasks = (await taskService.getByField(
+        'done',
+        +req.query.from,
+        +req.query.to,
+        'status'
+      )).hits.hits;
+
+      const ratios = doneTasks.map(task => {
+        const sourceTask: any = task._source;
+        const due = new Date(sourceTask.due).getTime();
+        let minAssignDate = new Date(
+          sourceTask.assignUpdates[0].created
+        ).getTime();
+        let maxStatusDate = new Date(
+          sourceTask.statusUpdates[0].created
+        ).getTime();
+
+        for (const update of sourceTask.assignUpdates) {
+          const currDate = new Date(update.created).getTime();
+          if (minAssignDate > currDate) {
+            minAssignDate = currDate;
+          }
+        }
+
+        for (const update of sourceTask.statusUpdates) {
+          const currDate = new Date(update.created).getTime();
+          if (maxStatusDate < currDate) {
+            maxStatusDate = currDate;
+          }
+        }
+
+        const ratio =
+          (Math.abs(maxStatusDate - minAssignDate) /
+            Math.abs(due - minAssignDate)) *
+          100;
+
+        return ratio;
+      });
+
+      const interval = this.averageDiff(ratios);
+      const groupedRatios: {
+        interval: number;
+        ratios: number[];
+      } = { interval, ratios: [] };
+
+      for (const ratio of ratios) {
+        // TODO: initiate to 0 and add one.
+        groupedRatios.ratios[Math.floor(ratio / interval)] = ratio;
+      }
+
+      return res.json(groupedRatios);
+    } catch (err) {
+      errorLogger.error('%j', {
+        message: err.message,
+        stack: err.stack,
+        name: err.name,
+      });
+      return res.status(500);
+    }
+  }
+
+  private static averageDiff(collection: number[]) {
+    return (
+      collection.reduce((sum: number = 0, value: number, i: number) => {
+        return (
+          sum + (i === 0 ? collection[i] : collection[i] - collection[i - 1])
+        );
+      }) / collection.length
+    );
   }
 }
