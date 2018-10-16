fa8b6771c0dc0ba75148d6e820c82e97a2ac688f
Omer <zamir98@gmail.com>
2018-10-16 10:16:37 +0300

    standards

--- src/auth/passport.ts
+++ src/auth/passport.ts
@@ -1,35 +1,37 @@
-import passport, { Profile } from 'passport';
-import { Application } from 'express';
+import passport, { Profile } from "passport";
+import { Application } from "express";
 
-import userModel from '../users/user.model';
-import IUser from '../users/user.interface';
-import LocalStrategy from './passport.local';
+import userModel from "../users/user.model";
+import Iuser from "../users/user.interface";
+import LocalStrategy from "./passport.local";
 
-export const authenticate = passport.authenticate('local', { successRedirect: '/api/login/success',
-                                                    failureRedirect: '/api/login/fail',
-                                                    failureFlash: false });
+export const authenticate = passport.authenticate("local", {
+  successRedirect: "/api/login/success",
+  failureRedirect: "/api/login/fail",
+  failureFlash: false
+});
 
 export const init = (app: Application) => {
-    app.use(passport.initialize());
-    app.use(passport.session());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-    passport.serializeUser((user:Profile, done) => {
-        done(null, user.id);
-    });
+  passport.serializeUser((user: Profile, done) => {
+    done(null, user.id);
+  });
 
-    passport.deserializeUser((id, done) => {
-        userModel.findById(id, (err, user) => {
-            done(err, <IUser>user);
-        });
+  passport.deserializeUser((id, done) => {
+    userModel.findById(id, (err, user) => {
+      done(err, user as Iuser);
     });
+  });
 
-    passport.use(LocalStrategy);
+  passport.use(LocalStrategy);
 
-    app.post('/api/login', authenticate);
-    app.get('/api/login/success', (req, res) => {
-        return res.status(200).send({user:req.user});
-    });
-    app.get('/api/login/fail', (req, res) => {
-        return res.status(401);
-    });
+  app.post("/api/login", authenticate);
+  app.get("/api/login/success", (req, res) => {
+    return res.status(200).send({ user: req.user });
+  });
+  app.get("/api/login/fail", (req, res) => {
+    return res.status(401);
+  });
 };
--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -3,7 +3,7 @@ import infoLogger from "../loggers/info.logger";
 import errorLogger from "../loggers/error.logger";
 import { Request, Response, NextFunction } from "express";
 
-export default class taskController {
+export default class TaskController {
   /**
    * validates queries and fetch the count of tasks per interval as given by a given field in given time range.
    *
@@ -11,7 +11,7 @@ export default class taskController {
    * @param {Response} res
    * @param {NextFunction} next
    */
-  static async getFieldCountPerInterval(
+  public static async getFieldCountPerInterval(
     req: Request,
     res: Response,
     next: NextFunction
@@ -37,8 +37,8 @@ export default class taskController {
         req.query.interval
       );
 
-      res.json(response.aggregations["1"]["buckets"]);
-      return next(response.aggregations["1"]["buckets"]);
+      res.json(response.aggregations["1"].buckets);
+      return next(response.aggregations["1"].buckets);
     } catch (err) {
       errorLogger.error("%j", {
         message: err.message,
@@ -57,7 +57,7 @@ export default class taskController {
    * @param {Response} res
    * @param {NextFunction} next
    */
-  static async getCountByStatus(
+  public static async getCountByStatus(
     req: Request,
     res: Response,
     next: NextFunction
@@ -77,8 +77,8 @@ export default class taskController {
         +req.query.to
       );
 
-      res.json(response.aggregations["1"]["buckets"]);
-      return next(response.aggregations["1"]["buckets"]);
+      res.json(response.aggregations["1"].buckets);
+      return next(response.aggregations["1"].buckets);
     } catch (err) {
       errorLogger.error("%j", {
         message: err.message,
@@ -97,7 +97,11 @@ export default class taskController {
    * @param {Response} res
    * @param {NextFunction} next
    */
-  static async getTagCloud(req: Request, res: Response, next: NextFunction) {
+  public static async getTagCloud(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
     try {
       if (!req.query.from || !req.query.to) {
         res.status(400);
@@ -120,8 +124,8 @@ export default class taskController {
         size
       );
 
-      res.json(response.aggregations["1"]["buckets"]);
-      return next(response.aggregations["1"]["buckets"]);
+      res.json(response.aggregations["1"].buckets);
+      return next(response.aggregations["1"].buckets);
     } catch (err) {
       errorLogger.error("%j", {
         message: err.message,
@@ -133,40 +137,50 @@ export default class taskController {
     }
   }
 
-    /**
-     * validates queries and fetch the leaderboard data in a given time range.
-     * 
-     * @param {Request} req
-     * @param {Response} res
-     * @param {NextFunction} next
-     */
-    static async getLeaderboard(req: Request, res: Response, next: NextFunction) {
-        try {
-            if (!req.query.from || !req.query.to) {
-                res.status(400);
-                return next();
-            }
-            if (isNaN(req.query.from) || isNaN(req.query.to) || (req.query.size && isNaN(req.query.size))) {
-                res.status(400);
-                return next();
-            }
+  /**
+   * validates queries and fetch the leaderboard data in a given time range.
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   */
+  public static async getLeaderboard(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      if (!req.query.from || !req.query.to) {
+        res.status(400);
+        return next();
+      }
+      if (
+        isNaN(req.query.from) ||
+        isNaN(req.query.to) ||
+        (req.query.size && isNaN(req.query.size))
+      ) {
+        res.status(400);
+        return next();
+      }
 
-            const size = req.query.size ? +req.query.size : undefined;
+      const size = req.query.size ? +req.query.size : undefined;
 
-            const response = await taskService.getLeaderboard(+req.query.from, +req.query.to, size);
+      const response = await taskService.getLeaderboard(
+        +req.query.from,
+        +req.query.to,
+        size
+      );
 
-            res.json(response.aggregations['1']['buckets']);
-            return next(response.aggregations['1']['buckets']);
-        }
-        catch (err) {
-            errorLogger.error('%j', {
-                message: err.message,
-                stack: err.stack,
-                name: err.name
-            });
-            res.status(500);
-            next(err);
-        }
+      res.json(response.aggregations["1"].buckets);
+      return next(response.aggregations["1"].buckets);
+    } catch (err) {
+      errorLogger.error("%j", {
+        message: err.message,
+        stack: err.stack,
+        name: err.name
+      });
+      res.status(500);
+      next(err);
     }
+  }
 }
-
