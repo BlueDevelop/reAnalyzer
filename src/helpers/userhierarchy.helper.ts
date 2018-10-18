661cf48f4bcf6880a0fbd77d2414b7119d0e69b8
Omer <zamir98@gmail.com>
2018-10-18 11:26:46 +0300

    g

--- src/helpers/userhierarchy.helper.ts
+++ src/helpers/userhierarchy.helper.ts
@@ -1,6 +1,6 @@
 import getConfig from '../config';
 const config = getConfig();
-import * as _ from 'lodash';
+
 import axios from 'axios';
 import * as fs from 'fs';
 /**
@@ -9,25 +9,22 @@ import * as fs from 'fs';
  * @param {string} rootHierarchy return the ids\emails of all the users under this hierarchy
  */
 async function getHierarchy(rootHierarchy?: string) {
+  let data;
   if (config.hierarchyServiceAddrGetMembers && rootHierarchy) {
     // if theres an api to get the hirerchy from and a root hirerchy to get then make then make the request to the api
     const getURL: string = config.hierarchyServiceAddrGetMembers(rootHierarchy);
     const response = await axios.get(getURL);
-    const resData = response.data;
-
-    const idList = _.map(resData, person => person.id);
-    return idList;
+    data = response.data;
   } else if (config.hierarchyServiceMockFile) {
     // use a static mock json file
-    const data = JSON.parse(
-      fs.readFileSync(config.hierarchyServiceMockFile, 'utf8')
-    );
-    const idList = _.map(data, person => person.id);
-    return idList;
+    data = JSON.parse(fs.readFileSync(config.hierarchyServiceMockFile, 'utf8'));
   } else {
     // TODO:throw error or do nothing
     throw new Error('hierarchy not configured');
   }
+  return data.map((person: any) => {
+    return person.id;
+  });
 }
 
 const hierarchyService = { getHierarchy };
