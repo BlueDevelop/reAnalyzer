d152c2ecb198c2d723f961a43d4a32a947e2cfc1
Omri <omrihaber@me.com>
2018-10-21 08:24:54 +0300

    user hierarchy service

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,14 @@ interface IConfig {
   apmAdress: string;
   apmServiceName: string;
   elasticsearch: string;
-  hierarchyServiceAddr?: string;
-  hierarchyServiceMockFile?: string;
-  hierarchyServiceAddrGetMembers?: (hierarchy: string) => string;
+  hierarchyServiceUseMock: boolean; // if true will use mockfile to instead of api
+  hierarchyServiceAddr?: string; // the address of the users\hierarchies API
+  hierarchyServiceMockFile?: string; // local file describing return value for each hierarchy id
+  hierarchyServiceGroupMembersFile?: string; // local file describing each groups members,
+  hierarchyFile?: string; // local file describing indirect sub groups of a each group
+  hierarchyUserIDToHierarchyFile?: string; // local file containing a mapping between userIDS and its hierarchy
+  hierarchyServiceAddrGetMembers?: (hierarchy: string) => string; // function that generates the route to get all members\users under a given hierarchy from the api
+  hierarchyServiceAddrGetUser?: (userID: string) => string; // function that generates the route to get a single user\member info
 }
 
 const prod: IConfig = {
@@ -22,9 +27,13 @@ const prod: IConfig = {
   apmAdress: "http://apm:8200",
   apmServiceName: "reAnalyzer_prod",
   elasticsearch: "http://elasticsearch:9200",
-  hierarchyServiceAddr: "http://hierarchyServiceAddress/api/hierarchy/",
+  hierarchyServiceAddr: "http://hierarchyServiceAddress/api/",
+  hierarchyFile: "./mock/hierarchy.json",
+  hierarchyServiceUseMock: false,
   hierarchyServiceAddrGetMembers: (hierarchyID: string) =>
-    `${prod.hierarchyServiceAddr}${hierarchyID}/members`
+    `${prod.hierarchyServiceAddr}hierarchy/${hierarchyID}/members`,
+  hierarchyServiceAddrGetUser: (userID: string) =>
+    `${prod.hierarchyServiceAddr}user/${userID}`
 };
 
 const dev: IConfig = {
@@ -35,7 +44,9 @@ const dev: IConfig = {
   apmAdress: "http://apm:8200",
   apmServiceName: "reAnalyzer_dev",
   elasticsearch: "http://elasticsearch:9200",
-  hierarchyServiceMockFile: "./mock/members.json"
+  hierarchyServiceMockFile: "./mock/members.json",
+  hierarchyServiceUseMock: true,
+  hierarchyUserIDToHierarchyFile: "./mock/userIDToHierarchy.json"
 };
 
 const test: IConfig = {
@@ -45,7 +56,8 @@ const test: IConfig = {
   logLevel: "verbose",
   apmAdress: "http://localhost:8200",
   apmServiceName: "reAnalyzer_test",
-  elasticsearch: "http://localhost:9200"
+  elasticsearch: "http://localhost:9200",
+  hierarchyServiceUseMock: false
 };
 
 export default () => {
